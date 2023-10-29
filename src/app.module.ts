ba0a1c5c9e12829ad775c0746d831109e8ef3950
Salomon AYAH <salomonayah@gmail.com>
2023-10-29 12:54:12 +0100

    project resources created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './customer/customer.module';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [CustomerModule, UserModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getServerActive(): string {
+    return 'The REST API server is active!';
   }
 }
